efc30d11c4bf9996a8c57d9af6550568c5b21ff7
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-02-20 20:50:33 -0300

    Criando a function handleClick

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ class Board extends React.Component {
         squares: Array(9).fill(null),
       };
     }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares});
+    }
   
     renderSquare(i) {
       return (
