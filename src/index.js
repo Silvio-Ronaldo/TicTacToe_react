289443b93d89fd9e2995e025e8f2ac9f55e2474c
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-02-20 19:37:51 -0300

    Passando a responsabilidade do state para o Board

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: null,
-      };
-    }
-
-    render() {
+   render() {
       return (
         <button 
           className="square" 
-          onClick={ () => this.setState({ value: 'X'}) }
+          onClick={ () => this.props.onClick() }
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -31,7 +24,12 @@ class Board extends React.Component {
     }
   
     renderSquare(i) {
-      return <Square value={i} />;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
