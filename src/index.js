a23c2437f24fc364339c7d2605d46a46d75d6c93
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-02-21 02:13:08 -0300

    Adicionando uma lista de jogadas anteriores

--- src/index.js
+++ src/index.js
@@ -76,6 +76,15 @@ class Game extends React.Component {
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
 
+      const moves = history.map((step, move) => {
+        const desc = move ? `Go to move # ${move}` : `Go to game start`;
+        return (
+          <li>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
       let status;
       if(winner) {
         status = `Winner: ${winner}`;
@@ -93,7 +102,7 @@ class Game extends React.Component {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
